99e2933d8f191816ab20a46f0c01d13121cead6a
shunpei <e1n22006@oit.ac.jp>
2025-07-01 16:17:28 +0900

    XXXX

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -7,9 +7,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
+    @Test
+    public void testAppReturnHelloWorld() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertEquals("Hello World!", classUnderTest.getGreeting());
     }
 }
 
